a334a70f55b04c429ee64109813ad9aed26dd5f7
alkasm1 <shamwebs1@gmail.com>
2025-07-19 18:37:30 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,19 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("keynova-cache").then(cache => {
-      return cache.addAll([
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("keynova-cache").then(cache =>
+      cache.addAll([
         "/",
         "/index.html",
+        "/manifest.json",
         "/bg2.png",
-        "/icon.png",
-        "/manifest.json"
-      ]);
-    })
+        "/icon.png"
+      ])
+    )
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
